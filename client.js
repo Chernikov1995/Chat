6b3bbb92607ec338609ba421e2430c86ab478406
Evgeny Chernikov <chernikovevge@gmail.com>
2020-11-11 22:09:20 +0300

    new version

--- client.js
+++ client.js
@@ -1,75 +1,79 @@
 const host = '127.0.0.1'
 const port = 8000
-const userName = `user${Math.floor((Math.random() * 10))}`
-const button = document.querySelector('#send')
-const messages = document.querySelector('#messages')
-const input = document.querySelector('#input')
+const userName = `user${~~(Math.random() * 10)}`
+const button = document.querySelector(".sendmessage")
+const messages = document.querySelector(".outputmessages")
+const input = document.querySelector(".inputmessage")
 
-const getRandomId = (min, max) => {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min) + min)
-}
+const set = new Set()
 
-async function postMethod (message) {
-  try {
-    const response = await fetch('http://127.0.0.1:8000/api/post', {
-      method: 'POST',
-      body: JSON.stringify({
-        ...message,
-        id: `${getRandomId(1, 10000000)}`
-      })
-    })
-    if (response.ok) {
-      const messages = await response.json()
-      console.log(messages)
-    }
-  } catch (e) {
-    console.log(e)
-  }
+async function postMethod (message){
+
+    try{
+        const response = await fetch('http://127.0.0.1:8000/api/post',{
+            method : 'POST',
+            body: JSON.stringify({
+                ...message
+             })
+            })
+        if (response.ok) {
+            const messages = await response.json()
+            console.log(messages)
+            }
+       } catch(e){
+            console.log(e)
+            }
 }
 
 async function getMethod() {
-  try {
-    const response = await fetch('http://127.0.0.1:8000/api/get')
-    if (response.ok) {
-      return response.json()
+    try{
+        const response = await fetch('http://127.0.0.1:8000/api/get')
+        if (response.ok){
+            return response.json()
+        }
+    }catch(e){
+        console.log(e)
     }
-  } catch (e) {
-    console.log(e)
-  }
 }
 
-setInterval(async () => {
-  const res = await getMethod()
-  const messageArray = res.msg
-  console.log(messageArray)
-  const showedMessages = [...messages.children || []].map(node => node.id)
-  const notShowedMessages = messageArray.filter(msg => !showedMessages.includes(msg.id))
-  for (const msg of notShowedMessages) {
-    const messageHtml = `<p> ${msg.user} : ${msg.text} </p>`
-    const msgNode = document.createElement('div')
-    msgNode.innerHTML = messageHtml
-    msgNode.id = msg.id
-    messages.append(msgNode)
-  }
-
-}, 100)
-
-const buttonHandler = async (event) => {
-  event.preventDefault()
-  if (input.value === ''){
-    alert('Incorrect Input')
-  } else {
-    const msg = {
-      user: userName,
-      text: input.value
+setInterval(async ()=>{
+    const res = await getMethod()
+    const messageArray = res.msg
+    for (const msg of messageArray) {
+        if (!set.has(msg.date)) {
+            const messageHtml = `<p> ${msg.user} : ${msg.text} </p>`
+            const msgNode = document.createElement('div')
+            msgNode.innerHTML = messageHtml
+            messages.append(msgNode)
+            set.add(msg.date)
+        }
     }
-    await postMethod(msg)
-    input.value = ''
-  }
+
+},100)
+
+
+const buttonHandler = async (event) =>{
+    event.preventDefault()
+    if (input.value === ''){
+     alert('Incorrect input')
+    } else{
+        const msg = {
+            user : userName,
+            text : input.value,
+            date : Date.now()
+        }
+        await postMethod(msg)
+        input.value = ""
+    }
 
 }
 
-button.addEventListener('click',buttonHandler)
+document.addEventListener('keydown',  (event)  => {
+    if (event.key === 'Enter') {
+        buttonHandler(event)
+    }
+})
+
+
+button.addEventListener('click', buttonHandler)
 
--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const http = require('http')
-const messageHistorySize = 10
+const messageBuffersize = 100
 const host = '127.0.0.1'
 const port = 8000
-const messages = []
-
+let messages = []
 
 const requestListener = (request,response) => {
+
     if (request.method === 'POST' && request.url === '/api/post') {
         let body = []
         request.on('data', (chunk) => {
             body.push(chunk)
         }).on('end', () => {
-            const tojson = Buffer.concat(body).toString()
-            const msg = JSON.parse(tojson)
-            if (messages.length >= messageHistorySize){
-                messages.shift()
+            const json = Buffer.concat(body).toString()
+            const msg = JSON.parse(json)
+            if (messages.length >= messageBufferSize){
+                messages = messages.slice(50)
             }
             messages.push(msg)
-            console.log(messages)
             response.setHeader('Content-Type', 'application/json')
             response.setHeader('Access-Control-Allow-Origin', '*')
-            response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept')
+            response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept,')
             response.status = 200
             response.end(JSON.stringify({
                 status: 'ok'
@@ -32,10 +31,10 @@ const requestListener = (request,response) => {
     if (request.method === 'GET' && request.url === '/api/get') {
         response.setHeader('Content-Type', 'application/json')
         response.setHeader('Access-Control-Allow-Origin', '*')
-        response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept')
+        response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept,')
         response.status = 200
         response.end(JSON.stringify({
-            msg: messages
+            msg: messages,
         }))
     }
 }
